7ab9589221f8eb83391eb6bf65ba06fec86ddfcb
Atom <aarya_cs_2019@rkgitm.edu.in>
2025-03-23 09:43:01 +0530

    correlationID

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,11 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
-    System.err.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
+    final Logger log = LoggerFactory.getLogger(Main.class);
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 9092;
@@ -20,15 +16,18 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
+      OutputStream out = clientSocket.getOutputStream();
+      out.write(new byte[] { 0, 1, 2, 3 });
+      out.write(new byte[] { 0, 0, 0, 7 });
     } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
+      log.info("IOException: " + e.getMessage());
     } finally {
       try {
         if (clientSocket != null) {
           clientSocket.close();
         }
       } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+        log.info("IOException: " + e.getMessage());
       }
     }
   }
